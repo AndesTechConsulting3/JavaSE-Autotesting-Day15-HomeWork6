a2f4922503223da355b6c5470099413865064a9d
a0z9 <a0z9@rambler.ru>
2019-02-27 10:14:21 +0300

    1

---
+++ src/test/java/org/andestech/learning/rfb19/g3/ChromeAppTest.java
@@ -0,0 +1,39 @@
+package org.andestech.learning.rfb19.g3;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class ChromeAppTest
+{
+    private WebDriver wd = null;
+
+    @BeforeClass
+    public void initData(){
+    System.setProperty("webdriver.chrome.driver",
+            "E:\\drivers\\selenium\\chromedriver.exe");  // укажите свой каталог, где находится драйвер
+    System.out.println("+++ Class: " + this);
+
+    }
+
+    @Test
+    public void testCaseChrome01()
+    {
+        wd = new ChromeDriver();
+        wd.get("http://google.com");
+        assertTrue( true );
+    }
+
+
+    @AfterClass
+    public void tearDown()
+    {
+      if(wd != null) wd.quit();
+      System.out.println("--- Class: " + this);
+    }
+
+}
