f1984da9ede4e0529b443a266a1967fd608a2786
abdulagaevarf <elena.abdulagaeva@raiffeisen.ru>
2019-02-28 02:04:43 +0300

    homework

---
+++ src/test/java/org/andestech/learning/rfb19/g3/IEAppTest.java
@@ -0,0 +1,40 @@
+package org.andestech.learning.rfb19.g3;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class IEAppTest
+{
+    private WebDriver wd = null;
+
+    @BeforeClass
+    public void initData(){
+    System.setProperty("webdriver.ie.driver",
+            "C:\\IEDriverServer_x64_3.14.0\\IEDriverServer.exe");  // укажите свой каталог, где находится драйвер
+    System.out.println("+++ Class: " + this);
+
+    }
+
+    @Test
+    public void testCaseIE01()
+    {
+        wd = new InternetExplorerDriver();
+        wd.get("http://google.com");
+        assertTrue( true );
+    }
+
+
+    @AfterClass
+    public void tearDown()
+    {
+      if(wd != null) wd.quit();
+      System.out.println("--- Class: " + this);
+    }
+
+}
