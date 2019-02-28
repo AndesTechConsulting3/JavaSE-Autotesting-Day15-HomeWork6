4e2da23667b7ed7a806d2bc4be1ea8cfd7fff52f
AntonB <mortician233@gmail.com>
2019-02-28 09:16:09 +0300

    homeWork

---
+++ src/test/java/org/andestech/learning/rfb19/g3/FirefoxAppTest.java
@@ -0,0 +1,37 @@
+package org.andestech.learning.rfb19.g3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class FirefoxAppTest {
+    private WebDriver wd = null;
+
+    @BeforeClass
+    public void initData(){
+        System.setProperty("webdriver.gecko.driver",
+                "lib/geckodriver.exe");  // укажите свой каталог, где находится драйвер
+        System.out.println("+++ Class: " + this);
+
+    }
+
+    @Test
+    public void testCaseChrome01()
+    {
+        wd = new FirefoxDriver();
+        wd.get("http://google.com");
+        assertTrue( true );
+    }
+
+
+    @AfterClass
+    public void tearDown()
+    {
+        if(wd != null) wd.quit();
+        System.out.println("--- Class: " + this);
+    }
+}
