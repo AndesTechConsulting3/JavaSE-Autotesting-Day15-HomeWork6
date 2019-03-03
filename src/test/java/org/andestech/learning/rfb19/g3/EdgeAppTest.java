fbaa814719f2beeaa2ff5558a2b24344d1f8bd25
rvnzzz <elenushkaterentjeva@gmail.com>
2019-03-03 21:44:14 +0300

    2

--- src/test/java/org/andestech/learning/rfb19/g3/EdgeAppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/EdgeAppTest.java
@@ -3,26 +3,32 @@ package org.andestech.learning.rfb19.g3;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.assertTrue;
 
-public class ChromeAppTest
+public class EdgeAppTest
 {
     private WebDriver wd = null;
 
     @BeforeClass
     public void initData(){
-    System.setProperty("webdriver.chrome.driver",
-            "D:\\drivers\\chromedriver.exe");  // укажите свой каталог, где находится драйвер
+    System.setProperty("java.net.preferIPv4Stack", "true");
+    System.setProperty("webdriver.ie.driver",
+            "D:\\drivers\\IEDriverServer.exe");  // укажите свой каталог, где находится драйвер
     System.out.println("+++ Class: " + this);
 
     }
 
     @Test
     public void testCaseChrome01() throws InterruptedException {
-        wd = new ChromeDriver();
+        wd = new InternetExplorerDriver();
         wd.get("http://google.com");
         assertTrue( true );
         Thread.sleep(3000);
--- src/test/java/org/andestech/learning/rfb19/g3/FirefoxAppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/FirefoxAppTest.java
@@ -3,26 +3,28 @@ package org.andestech.learning.rfb19.g3;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertTrue;
 
-public class ChromeAppTest
+public class FirefoxAppTest
 {
     private WebDriver wd = null;
 
     @BeforeClass
     public void initData(){
-    System.setProperty("webdriver.chrome.driver",
-            "D:\\drivers\\chromedriver.exe");  // укажите свой каталог, где находится драйвер
+    System.setProperty("webdriver.gecko.driver",
+            "D:\\drivers\\geckodriver.exe");  // укажите свой каталог, где находится драйвер
     System.out.println("+++ Class: " + this);
 
     }
 
     @Test
     public void testCaseChrome01() throws InterruptedException {
-        wd = new ChromeDriver();
+        wd = new FirefoxDriver();
         wd.get("http://google.com");
         assertTrue( true );
         Thread.sleep(3000);
