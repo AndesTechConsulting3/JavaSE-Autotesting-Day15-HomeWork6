ce2fced0cf5627e0a23cdef8757699c98a33a98c
ruatde6 <Evgeny.TODARENKO@raiffeisen.ru>
2019-03-04 12:25:06 +0300

    Не сделал запуск IE потому что работаю на макбуке

--- src/test/java/org/andestech/learning/rfb19/g3/FirefoxAppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/FirefoxAppTest.java
@@ -1,31 +1,30 @@
 package org.andestech.learning.rfb19.g3;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class ChromeAppTest
-{
+public class FirefoxAppTest {
+
     private WebDriver wd = null;
 
     @BeforeClass
     public void initData(){
-    System.setProperty("webdriver.chrome.driver",
-            "drivers/chromedriver");  // укажите свой каталог, где находится драйвер
-    System.out.println("+++ Class: " + this);
+        System.setProperty("webdriver.gecko.driver",
+                "drivers/geckodriver");  // укажите свой каталог, где находится драйвер
+        System.out.println("+++ Class: " + this);
 
     }
 
     @Test
     public void testCaseChrome01()
     {
-        wd = new ChromeDriver();
+        wd = new FirefoxDriver();
         wd.get("http://google.com");
 
         WebElement $googleImage = wd.findElement(By.cssSelector("img[alt=Google]"));
@@ -37,8 +36,7 @@ public class ChromeAppTest
     @AfterClass
     public void tearDown()
     {
-      if(wd != null) wd.quit();
-      System.out.println("--- Class: " + this);
+        if(wd != null) wd.quit();
+        System.out.println("--- Class: " + this);
     }
-
 }
---
+++ src/test/java/org/andestech/learning/rfb19/g3/IEAppTest.java
@@ -0,0 +1,6 @@
+package org.andestech.learning.rfb19.g3;
+
+public class IEAppTest {
+
+    //Не могу сделать, так как выполняю домашнюю работу на макбуке, на нем отсутствует IE
+}
